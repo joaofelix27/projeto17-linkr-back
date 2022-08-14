0e14aa338b3ebb5b5d72cebede4cd219850d8447
jvcamargo02 <joaovcamargo2002@gmail.com>
2022-08-14 14:42:34 -0300

    Fix: res.send hashtag router

--- src/routes/hashtagRouter.js
+++ src/routes/hashtagRouter.js
@@ -1,10 +1,13 @@
-import { Router } from 'express';
-import { getHashtagByName, getTrending } from '../controllers/hashtagController.js';
+import { Router } from "express";
+import {
+    getHashtagByName,
+    getTrending,
+} from "../controllers/hashtagController.js";
+import getUserData from "../middlewares/getUserData.js";
 
 const hashtagRouter = Router();
 
-hashtagRouter.get('/hashtags/:name',getHashtagByName);
-hashtagRouter.get('/trending',getTrending);
+hashtagRouter.get("/hashtags/:name", getUserData, getHashtagByName);
+hashtagRouter.get("/trending", getTrending);
 
 export default hashtagRouter;
-
