a60828e49e616084d758a8c6d1dee18344c8580e
Renan Araujo <ra.carvalho@unesp.br>
2022-08-15 18:30:11 -0300

    seach user by id

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -8,4 +8,16 @@ export async function searchUser(req, res) {
     } catch (error) {
         res.sendStatus(500);
     }
+};
+
+export async function getUserById(req,res) {
+    const {id} = req.params;
+    try {
+        const {rows: users} = await userRepository.getUserById(id);
+        if(users.length === 0) return res.sendStatys(404);
+        res.status(200).send(users[0]);
+    }catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
 }
--- src/routes/searchRouter.js
+++ src/routes/searchRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { searchUser } from "../controllers/searchController.js";
+import { getUserById, searchUser } from "../controllers/searchController.js";
 
 const searchRouter = Router();
 
+searchRouter.get("/user/:id", getUserById);
 searchRouter.post("/timeline/user", searchUser);
 
 export default searchRouter;
