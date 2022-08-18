392a01e71005f1c9f9eb509f4d4dda685322a20c
joaofelix27 <joaofelix27@gmail.com>
2022-08-18 03:38:53 -0300

    Query seachUsers updated to show following people first on the searchBox

--- src/routes/searchRouter.js
+++ src/routes/searchRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import getUserData from "../middlewares/getUserData.js";
 import { getUserById, searchUser } from "../controllers/searchController.js";
 
 const searchRouter = Router();
 
 searchRouter.get("/user/:id", getUserById);
-searchRouter.post("/timeline/user", searchUser);
+searchRouter.post("/timeline/user",getUserData, searchUser);
 
 export default searchRouter;
